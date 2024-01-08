190488bca39cdc2268d5e1e09cdb1183f7e79832
Jérôme Gorin <gorinje@gmail.com>
2024-01-08 10:23:37 +0100

    Update filters licence

--- bmp/qdbmp.c
+++ bmp/qdbmp.c
@@ -1,4 +1,4 @@
-/* BEVARA
+/* 
 **
 ** Adapted from the qdbmp code; see QDBMP license information below
 **
@@ -6,6 +6,13 @@
 ** 32, 24, and 8bpp (non-indexed) to RGBA format. Later implementations should handle other, less popular, BMP versions.
 **
 **
+** This file is part of Bevara Access Filters.
+** 
+** This file is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation.
+** 
+** This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License along with this file. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <gpac/filters.h>
--- common/convert.c
+++ common/convert.c
@@ -1,3 +1,15 @@
+/* 
+**
+** This file is part of Bevara Access Filters.
+** 
+** This file is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation.
+** 
+** This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public License along with this file. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #include <gpac/filters.h>
 
 void convert_rgb_to_rgba(char* rgba, const char* rgb, u32 count) {
